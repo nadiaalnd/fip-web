4d497cfd99d8dedbcee6187b112f12b0791bf6d1
ade.hafis <ade.hafis@socialbread.id>
2023-08-17 17:13:50 +0700

    OK DOL

--- test-case/investment.js
+++ test-case/investment.js
@@ -91,12 +91,115 @@ function calculateResult(input) {
 // Example input
 const input = {
   target_money: 10000000,
-  target_year: 5,
+  target_year: 1,
   initial_money: 1000000,
-  investment_periode: "annually",
-  investment: 83333.3333333,
+  investment_periode: "monthly",
+  investment: 100000,
   interest: 10,
 };
 
-const result = calculateResult(input);
+function calculateInvestment(input) {
+  const {
+    target_money,
+    target_year,
+    initial_money,
+    investment_periode,
+    investment,
+    interest,
+  } = input;
+
+  let temp_money = initial_money;
+  let isSuccess = false;
+  let invest_total = 0;
+  for (let i = 0; i < target_year; i++) {
+    temp_money =
+      investment_periode == "annually"
+        ? calculateMoneyPerYear(temp_money, investment)
+        : calculateMoneyPerMonth(temp_money, investment);
+    let tmp = getReturnOnInvestment(temp_money, interest, investment_periode);
+    temp_money += tmp;
+    invest_total += tmp;
+  }
+
+  isSuccess = temp_money >= target_money ?? false;
+  let results = {
+    success: isSuccess,
+    invest_total: temp_money,
+    invest_interest: invest_total,
+    invest_primary: temp_money - invest_total,
+  };
+
+  if (!isSuccess) {
+    const recommendation = gettingRecommendationYearInvestment(
+      target_money,
+      initial_money,
+      investment_periode,
+      investment,
+      interest
+    );
+    results = {
+      ...results,
+      recommendation,
+    };
+  }
+
+  return results;
+}
+
+function gettingRecommendationYearInvestment(
+  target_money,
+  initial_money,
+  investment_periode,
+  investment,
+  interest
+) {
+  let temp_money = initial_money;
+  let isSuccess = false;
+  let invest_total = 0;
+  let year = 0;
+  while (!isSuccess) {
+    temp_money =
+      investment_periode == "annually"
+        ? calculateMoneyPerYear(temp_money, investment)
+        : calculateMoneyPerMonth(temp_money, investment);
+    let tmp = getReturnOnInvestment(temp_money, interest, investment_periode);
+    temp_money += tmp;
+    invest_total += tmp;
+    year++;
+    isSuccess = temp_money >= target_money ?? false;
+  }
+
+  return {
+    recommendation_year: monthToYear(year, investment_periode),
+    invest_total: temp_money,
+    invest_interest: invest_total,
+    invest_primary: temp_money - invest_total,
+  };
+}
+
+function monthToYear(month, type = "annually") {
+  return type === "monthly" ? month / 12 : month;
+}
+
+function calculateMoneyPerYear(init_money, investment) {
+  return investment * 12 + init_money;
+}
+
+function calculateMoneyPerMonth(init_money, investment) {
+  return investment + init_money;
+}
+
+function getReturnOnInvestment(
+  init_money,
+  interest,
+  investment_periode = "annually"
+) {
+  if (investment_periode === "annually") {
+    return init_money * (interest / 100);
+  } else if (investment_periode === "monthly") {
+    return (init_money * (interest / 100)) / 12;
+  }
+}
+
+const result = calculateInvestment(input);
 console.log(result);
